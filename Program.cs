5c253fe101680d5dfe6feedae0d9f88d52fd5466
mlrainer <112909069+mlrainer@users.noreply.github.com>
2022-11-06 15:00:06 -0500

    clear

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LegoSets
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("How many records do you want to add? ");
+#pragma warning disable CS8604 // Possible null reference argument.
+            var numberOfRecords = int.Parse(Console.ReadLine());
+#pragma warning restore CS8604 // Possible null reference argument.
+
+            var recordList = new List<SetName>();
+            for (int i = 0; i < numberOfRecords; i++)
+            {
+
+                SetName setname = new();
+                setname.Id = i + 1;
+
+                Console.WriteLine("Enter sets Title:");
+                setname.ThemeSubTitle = (Console.ReadLine());
+
+                Console.WriteLine("Enter sets Theme):");
+                setname.Theme = (Console.ReadLine());
+
+                Console.WriteLine("Enter sets number of pieces:");
+                setname.Pieces = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter sets number of Mini Figures:");
+                setname.MiniFigs = int.Parse(Console.ReadLine());
+
+                recordList.Add(setname);
+
+                Console.WriteLine("Printing Set Details");
+            }
+            foreach (SetName setname in recordList)
+            {
+                Console.WriteLine($"Lego Set '{setname.Id}'");
+                Console.WriteLine($"Theme Sub Title: {setname.ThemeSubTitle}");
+                Console.WriteLine($"Theme: {setname.Theme}");
+                Console.WriteLine($"Number of Pieces: {setname.Pieces}");
+                Console.WriteLine($"Number of Mini Figures: {setname.MiniFigs}");
+            }
+        }
+    }
+}
---
+++ SetDetails.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace LegoSets
+{
+    class SetDetails
+    {
+        public int Id { get; set; }
+        public string? Theme { get; set; }
+        public int Pieces { get; set; }
+        public int MiniFigs { get; set; }
+    }
+}
